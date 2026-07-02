06b01d51b047d676d2d001535b0f05ccd9efd901
amrit <vishal179singh@gmail.com>
2026-07-02 18:04:58 +0530

    github signin , server action  done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client"
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import Image from "next/image";
+import { authClient } from "@/lib/auth-client"
 
 export default function Home() {
+  const { data } = authClient.useSession()
+  console.log(" console log",data?.user)
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -61,7 +65,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <ModeToggle/>
+      <ModeToggle />
     </div>
   );
 }
