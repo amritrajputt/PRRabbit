c55fbb6cb2181b4271ed1c6382ddc63ce1755bb5
amrit <vishal179singh@gmail.com>
2026-07-06 19:39:39 +0530

    inngest route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/features/inngest/client";
 import { processTask } from "./functions";
+import { reviewPullRequest } from "@/features/reviews/server/review-pr-functions";
+import { syncRepoCodebaseFunction } from "@/features/repo-sync/server/repo-sync-function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [processTask],
+  functions: [processTask, reviewPullRequest, syncRepoCodebaseFunction],
 });
