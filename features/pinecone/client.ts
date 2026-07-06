706da3667c6164a7bfa61760d2b7075d7039401d
amrit <vishal179singh@gmail.com>
2026-07-06 18:52:35 +0530

    pinecone setup

---
+++ features/pinecone/client.ts
@@ -0,0 +1,12 @@
+import { Pinecone } from '@pinecone-database/pinecone';
+
+let pinecone :Pinecone|null = null
+export function getPineconeIndex(){
+    if(!pinecone){
+        pinecone = new Pinecone({
+            apiKey: process.env.PINECONE_API_KEY!,
+        });
+    }
+    const index = pinecone.index(process.env.PINECONE_INDEX!);
+    return index;
+}
